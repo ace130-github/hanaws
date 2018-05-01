e40c3330d5a67bcc05777fe24ec43a2614c98a2b
pblaesi <paul.blaesi@exxcellent.de>
2018-05-01 23:42:20 +0200

    add search box

--- ui5ui/resources/webapp/controller/runner/RunnerOverview.controller.js
+++ ui5ui/resources/webapp/controller/runner/RunnerOverview.controller.js
@@ -1,9 +1,17 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function(Controller) {
+	"sap/ui/core/mvc/Controller",
+  "sap/ui/model/json/JSONModel",
+  'sap/ui/model/Filter',
+  'sap/ui/model/FilterOperator'
+], function(Controller, JSONModel, Filter, FilterOperator) {
 	"use strict";
 
 	return Controller.extend("ui5ui.controller.runner.RunnerOverview", {
+
+		onInit: function() {
+			this.getView().setModel(new JSONModel(), 'search');
+		},
+
 		formatGenderIconURI: function(genderValue) {
 			if (genderValue === 'M') {
 				return 'sap-icon://customer';
@@ -19,6 +27,15 @@ sap.ui.define([
 			router.navTo("detail", {
 				runnerPath: event.getSource().getBindingContext("db").getPath().substring(1) // cut off the leading '/' which is not legal in a route parameter
 			});
-		}
+		},
+		
+		onSearch: function (event) {
+			var view = this.getView();
+			var searchTerm = view.getModel('search').getProperty('/searchTerm');
+			var filter = new Filter('name', FilterOperator.Contains, searchTerm);
+			var runnerList = view.byId('runnerList');
+			runnerList.getBinding('items').filter([filter]);
+
+    }
 	});
 });
